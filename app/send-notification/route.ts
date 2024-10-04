7a76b6d68a60ffd0fa559c8dc89e0cc2ec72c8a9
vignesh.muralimohan <vignesh.muralimohan@straive.com>
2024-10-04 12:51:29 +0530

    key fix

--- app/send-notification/route.ts
+++ app/send-notification/route.ts
@@ -4,10 +4,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 // Initialize Firebase Admin SDK
 if (!admin.apps.length) {
-  import("../../service_key.json").then((serviceAccount) => {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-    });
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    }),
   });
 }
 export async function POST(request: NextRequest) {
