e5ff22b973377f6cb71f82041c428493ece6b749
vignesh.muralimohan <vignesh.muralimohan@straive.com>
2024-10-04 12:40:01 +0530

    service.json fix

--- app/send-notification/route.ts
+++ app/send-notification/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 // Initialize Firebase Admin SDK
 if (!admin.apps.length) {
-  const serviceAccount = require("@/service_key.json");
+  const serviceAccount = require("../../service_key.json");
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
